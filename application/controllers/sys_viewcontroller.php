cfbbfa9cf37e2d8a754d9caf89348a5c93a293c4
agswissbix <108857221+agswissbix@users.noreply.github.com>
2023-07-31 09:15:37 +0200

    .

--- application/controllers/sys_viewcontroller.php
+++ application/controllers/sys_viewcontroller.php
@@ -16701,7 +16701,7 @@ GROUP BY user_contratti.recordid_
         if($presenze!=null)
         {
             $dipendente_id=$presenze['id'];
-            $url=domain_url()."jdocweb_test/index.php/sys_viewcontroller/test_sync_timbrature/$dipendente_id"; 
+            $url=domain_url()."bixdata/index.php/bix_datagateway_controller/custom_sync_rapportidilavoro//$dipendente_id"; 
             $feedback=file_get_contents($url);
             echo $feedback;
         }    
